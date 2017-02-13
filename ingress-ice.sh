2251c9fc951a98eb6c501814aa242895245aaa96
wposada <wilsonposada@me.com>
2017-02-13 11:06:14 -0500

    migits

--- ingress-ice.sh
+++ ingress-ice.sh
@@ -9,7 +9,8 @@ COUNT=""
 MACHINE_TYPE=`uname -m`
 
 PHANTOMJS="$SCRIPT_HOME/phantomjs"
-
+PATH_GITS="/Volumes/TOSHIBA EXT/Docker/gits"
+PATH_CONF="/Volumes/TOSHIBA EXT/Docker/ingress-ice/conf"
 if (command -v dialog >/dev/null)
 then
   DIALOG=${DIALOG=dialog}
@@ -38,7 +39,7 @@ launch() {
     ARGS="$1 $2"
     if [ ! $LOOP ]
     then
-      $PHANTOMJS --ignore-ssl-errors=true --ssl-protocol=any "$SCRIPT_HOME/ice/ice.js" $ARGS; cd /gits/3874aad99ad5ce803a6e482e7c7dc241;git pull;cp /conf/.iced_cookies /gits/3874aad99ad5ce803a6e482e7c7dc241/;echo "fin cp .ice";git add .;git commit -m "migits";git push;exit;
+      $PHANTOMJS --ignore-ssl-errors=true --ssl-protocol=any "$SCRIPT_HOME/ice/ice.js" $ARGS; cd $PATH_GITS/3874aad99ad5ce803a6e482e7c7dc241;git pull;cp $PATH_CONF/.iced_cookies $PATH_GITS/3874aad99ad5ce803a6e482e7c7dc241/;echo "fin cp .ice";git add .;git commit -m "migits";git push;exit;
     else
       while :; do
         $PHANTOMJS --ignore-ssl-errors=true f--ssl-protocol=any "$SCRIPT_HOME/ice/ice.js" $ARGS
